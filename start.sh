487e532981d85b4a65017afeecc176b558e48411
Donal Byrne <byrnedo@tcd.ie>
2016-04-06 16:34:48 +0200

    Fixed error when starting

--- start.sh
+++ start.sh
@@ -17,7 +17,9 @@ procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Again set the right permissions (needed when mounting from a volume)
+set +e 
 chown -Rf www-data.www-data /usr/share/nginx/html/
+set -e
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
