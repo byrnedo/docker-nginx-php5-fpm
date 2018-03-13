b43df5104c5f603e198b6d60d0d8bb1923eb89c3
Donal Byrne <donal@sendify.se>
2018-03-13 14:27:54 +0100

    updated startup cmd

--- cmd.sh
+++ cmd.sh
@@ -13,9 +13,9 @@ else
   sed -i -e "s/display_errors =.*/display_errors = Off/g" /etc/php/5.6/fpm/php.ini
 fi
 
-# Tweak nginx to match the workers to cpu's
-procs=$(cat /proc/cpuinfo |grep processor | wc -l)
-sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
+if [[ ! -z "$PHP_FPM_POOL_CONF" ]]; then
+    echo "$PHP_FPM_POOL_CONF" > /etc/php/5.6/fpm/pool.d/z-www.conf
+fi
 
 # Set the root in the conf
 sed -i -e "s#%%NGINX_ROOT%%#$NGINX_ROOT#" /etc/nginx/sites-available/default.conf
